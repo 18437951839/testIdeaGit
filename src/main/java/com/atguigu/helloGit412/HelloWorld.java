c34f8091544782fefb8544eadeb142a263f5f81e
yhn <851966667@qq.com>
2020-09-26 19:06:53 +0800

    测试idea提交测试分支合并

--- src/main/java/com/atguigu/helloGit412/HelloWorld.java
+++ src/main/java/com/atguigu/helloGit412/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("拉拉库！");
         System.out.println("hello!!");
+        System.out.println("今天是个好日子");
     }
 }
